5cf68fe353fc9a93eb7767d3ff159558253e54bf
Михаил Кривенко <mihan1235@yandex.ru>
2023-03-03 18:20:31 +0300

    fixup! Moved to folder for migration

--- src/test/resources/TestExecution/create_table_customers.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Customers
-(
-    CustomerName varchar(255),
-    ContactName  varchar(255),
-    Address      varchar(255),
-    City         varchar(255),
-    PostalCode   int,
-    Country      varchar(255)
-);

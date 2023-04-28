b6ba16ed16a5848e5fe15e83d57420f4c2e352e4
MK Mihan1235 <109580177+mihan1235@users.noreply.github.com>
2023-04-28 16:53:41 +0300

    fixup! fixup! fixup! Use json converter instead of protobufs

--- mixql-remote-messages/src/main/scala/org/mixql/protobuf/ProtoBufConverter.scala
+++ mixql-remote-messages/src/main/scala/org/mixql/protobuf/ProtoBufConverter.scala
@@ -1,13 +1,14 @@
 package org.mixql.protobuf
 
 import org.mixql.protobuf.messages
-import org.mixql.protobuf.messages.AnyMsg
+import org.mixql.protobuf.messages.{AnyMsg, Message}
 import io.circe.syntax._
 import io.circe.parser.decode
 import io.circe._
 import io.circe.generic.auto._
 import io.circe.syntax._
-import io.circe._, io.circe.parser._
+import io.circe._
+import io.circe.parser._
 import cats.syntax.either._
 
 import java.nio.charset.StandardCharsets
@@ -27,44 +28,114 @@ object ProtoBufConverter {
           exception.getMessage)
         case Success(v) => v
       }
-      {
-        anyMsg.`type` match {
-          case "org.mixql.protobuf.messages.messages.EngineName" =>
-            decode[messages.EngineName](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.ShutDown" =>
-            decode[messages.ShutDown](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.Execute" =>
-            decode[messages.Execute](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.Param" =>
-            decode[messages.Param](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.Error" =>
-            decode[messages.Error](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.SetParam" =>
-            decode[messages.SetParam](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.GetParam" =>
-            decode[messages.GetParam](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.IsParam" =>
-            decode[messages.IsParam](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.ParamWasSet" =>
-            decode[messages.ParamWasSet](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.ExecuteFunction" =>
-            decode[messages.ExecuteFunction](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.GetDefinedFunctions" =>
-            decode[messages.GetDefinedFunctions](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.DefinedFunctions" =>
-            decode[messages.DefinedFunctions](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.NULL" => decode[messages.NULL](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.Bool" => decode[messages.Bool](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.int" => decode[messages.int](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.double" => decode[messages.double](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.gString" => decode[messages.gString](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.gArray" => decode[messages.gArray](anyMsg.json).toTry
-          case "org.mixql.protobuf.messages.messages.Error" => decode[messages.Error](anyMsg.json).toTry
+      anyMsg.`type` match {
+        case "org.mixql.protobuf.messages.EngineName" =>
+          decode[messages.EngineName](anyMsg.json).toTry match {
+            case Failure(exception) => return messages.Error("error while unpacking from json EngineName: " +
+              exception.getMessage)
+            case Success(v) => v
+          }
+        case "org.mixql.protobuf.messages.ShutDown" =>
+          decode[messages.ShutDown](anyMsg.json).toTry match {
+            case Failure(exception) => return messages.Error("error while unpacking from json ShutDown: " +
+              exception.getMessage)
+            case Success(v) => v
+          }
+        case "org.mixql.protobuf.messages.Execute" =>
+          decode[messages.Execute](anyMsg.json).toTry match {
+            case Failure(exception) => return messages.Error("error while unpacking from json Execute: " +
+              exception.getMessage)
+            case Success(v) => v
+          }
+        case "org.mixql.protobuf.messages.Param" =>
+          decode[messages.Param](anyMsg.json).toTry match {
+            case Failure(exception) => return messages.Error("error while unpacking from json Param: " +
+              exception.getMessage)
+            case Success(v) => v
+          }
+        case "org.mixql.protobuf.messages.Error" =>
+          decode[messages.Error](anyMsg.json).toTry match {
+            case Failure(exception) => return messages.Error("error while unpacking from json Error: " +
+              exception.getMessage)
+            case Success(v) => v
+          }
+        case "org.mixql.protobuf.messages.SetParam" =>
+          decode[messages.SetParam](anyMsg.json).toTry match {
+            case Failure(exception) => return messages.Error("error while unpacking from json SetParam: " +
+              exception.getMessage)
+            case Success(v) => v
+          }
+        case "org.mixql.protobuf.messages.GetParam" =>
+          decode[messages.GetParam](anyMsg.json).toTry match {
+            case Failure(exception) => return messages.Error("error while unpacking from json GetParam: " +
+              exception.getMessage)
+            case Success(v) => v
+          }
+        case "org.mixql.protobuf.messages.IsParam" =>
+          decode[messages.IsParam](anyMsg.json).toTry match {
+            case Failure(exception) => return messages.Error("error while unpacking from json IsParam: " +
+              exception.getMessage)
+            case Success(v) => v
+          }
+        case "org.mixql.protobuf.messages.ParamWasSet" =>
+          decode[messages.ParamWasSet](anyMsg.json).toTry match {
+            case Failure(exception) => return messages.Error("error while unpacking from json ParamWasSet: " +
+              exception.getMessage)
+            case Success(v) => v
+          }
+        case "org.mixql.protobuf.messages.ExecuteFunction" =>
+          decode[messages.ExecuteFunction](anyMsg.json).toTry match {
+            case Failure(exception) => return messages.Error("error while unpacking from json ExecuteFunction: " +
+              exception.getMessage)
+            case Success(v) => v
+          }
+        case "org.mixql.protobuf.messages.GetDefinedFunctions" =>
+          decode[messages.GetDefinedFunctions](anyMsg.json).toTry match {
+            case Failure(exception) => return messages.Error("error while unpacking from json GetDefinedFunctions: " +
+              exception.getMessage)
+            case Success(v) => v
+          }
+        case "org.mixql.protobuf.messages.DefinedFunctions" =>
+          decode[messages.DefinedFunctions](anyMsg.json).toTry match {
+            case Failure(exception) => return messages.Error("error while unpacking from json DefinedFunctions: " +
+              exception.getMessage)
+            case Success(v) => v
+          }
+        case "org.mixql.protobuf.messages.NULL" => decode[messages.NULL](anyMsg.json).toTry match {
+          case Failure(exception) => return messages.Error("error while unpacking from json NULL: " +
+            exception.getMessage)
+          case Success(v) => v
         }
-      } match {
-        case Failure(ex) => messages.Error("error while unpacking jsonMsg of anyMsg: " +
-          ex.getMessage)
-        case Success(v) => v.asInstanceOf[messages.Message]
+        case "org.mixql.protobuf.messages.Bool" => decode[messages.Bool](anyMsg.json).toTry match {
+          case Failure(exception) => return messages.Error("error while unpacking from json Bool: " +
+            exception.getMessage)
+          case Success(v) => v
+        }
+        case "org.mixql.protobuf.messages.int" => decode[messages.int](anyMsg.json).toTry match {
+          case Failure(exception) => return messages.Error("error while unpacking from json int: " +
+            exception.getMessage)
+          case Success(v) => v
+        }
+        case "org.mixql.protobuf.messages.double" => decode[messages.double](anyMsg.json).toTry match {
+          case Failure(exception) => return messages.Error("error while unpacking from json double: " +
+            exception.getMessage)
+          case Success(v) => v
+        }
+        case "org.mixql.protobuf.messages.gString" => decode[messages.gString](anyMsg.json).toTry match {
+          case Failure(exception) => return messages.Error("error while unpacking from json gString: " +
+            exception.getMessage)
+          case Success(v) => v
+        }
+        case "org.mixql.protobuf.messages.gArray" => decode[messages.gArray](anyMsg.json).toTry match {
+          case Failure(exception) => return messages.Error("error while unpacking from json gArray: " +
+            exception.getMessage)
+          case Success(v) => v
+        }
+        case "org.mixql.protobuf.messages.Error" => decode[messages.Error](anyMsg.json).toTry match {
+          case Failure(exception) => return messages.Error("error while unpacking from json Error: " +
+            exception.getMessage)
+          case Success(v) => v
+        }
       }
     } catch {
       case e: Throwable =>

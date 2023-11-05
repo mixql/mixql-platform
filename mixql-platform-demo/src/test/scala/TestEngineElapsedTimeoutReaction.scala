25e26a207839d419a4c32ec3e3c8de798a16693e
mihan1235 <mihan1235@yandex.ru>
2023-11-05 15:54:36 +0300

    #23 Added test "should return failed tasks if if engine did not started and timeout elapsed during async execution"

--- mixql-platform-demo/src/test/scala/TestEngineElapsedTimeoutReaction.scala
+++ mixql-platform-demo/src/test/scala/TestEngineElapsedTimeoutReaction.scala
@@ -1,3 +1,5 @@
+import org.mixql.core.exception.MException
+
 class TestEngineElapsedTimeoutReaction extends MixQLClusterTest {
   import scala.concurrent.duration.Duration
   override val munitTimeout: Duration = Duration(100, "s")
@@ -12,6 +14,51 @@ class TestEngineElapsedTimeoutReaction extends MixQLClusterTest {
           |execute dummy command on dummy engine;
           |""".stripMargin)
     }
+  }
+
+  test("should return failed tasks if if engine did not started and timeout elapsed during async execution") {
+    run("""
+        |let engine dummy;
+        |let async1 = async
+        |  async1 statement1;
+        |  async1 statement2;
+        |  async1 statement3;
+        |  async1 statement4;
+        |  async1 statement5;
+        |  async1 statement6;
+        |  return none;
+        |end async;
+        |
+        |let async2 = async
+        |  async2 statement1;
+        |  async2 statement2;
+        |  async2 statement3;
+        |  async2 statement4;
+        |  async2 statement5;
+        |  async2 statement6;
+        |end async;
+        |
+        |let async3 = async
+        |  async3 statement1;
+        |  async3 statement2;
+        |  async3 statement3;
+        |  async3 statement4;
+        |  async3 statement5;
+        |  async3 statement6;
+        |end async;
+        |
+        |let res1, res2, res3 = await_all($async1, $async2, $async3);
+        |let engine "stub-local";
+        |""".stripMargin)
+    val res1 = context().getVar("res1")
+    assert(res1.isInstanceOf[MException])
+    assert(res1.asInstanceOf[MException].getMessage == "Broker: elapsed timeout for engine mixql-engine-dummy")
+    val res2 = context().getVar("res2")
+    assert(res2.isInstanceOf[MException])
+    assert(res2.asInstanceOf[MException].getMessage == "Broker: elapsed timeout for engine mixql-engine-dummy")
+    val res3 = context().getVar("res3")
+    assert(res3.isInstanceOf[MException])
+    assert(res3.asInstanceOf[MException].getMessage == "Broker: elapsed timeout for engine mixql-engine-dummy")
   }
 
   test("execute statements sequentially and no timeout error must appear") {

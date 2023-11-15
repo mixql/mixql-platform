c9150a53f419eb7a6ac47cb37495c5fe15d556fe
mihan1235 <mihan1235@yandex.ru>
2023-11-15 22:37:05 +0300

    Added "send error to platform, and it should stop" test

--- mixql-platform-demo/src/test/scala/TestSql1.scala
+++ mixql-platform-demo/src/test/scala/TestSql1.scala
@@ -4,4 +4,17 @@ class TestSql1TestSql1 extends MixQLClusterTest {
     import org.mixql.platform.demo.utils.FilesOperations
     run(FilesOperations.readFileContent(TestOps.getFileFromResource("test_sql1.sql").get))
   }
+
+  test("send error to platform, and it should stop") {
+    interceptMessage[java.lang.Exception](
+      "Module mixql-engine-sqlite: SQLightJDBC error while execute: " +
+        "[SQLITE_ERROR] SQL error or missing database (near \"rubish\": syntax error)"
+    ) {
+      run("""
+          |let engine sqlite;
+          |
+          |rubish comand;
+          |""".stripMargin)
+    }
+  }
 }
